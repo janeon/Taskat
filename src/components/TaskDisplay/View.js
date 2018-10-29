054ef60f5fe3189ddb85f4fb22c535207b2cda0f
janeon <jhsieh@oberlin.edu>
2018-10-29 10:34:51 -0400

    adding big-calendar in taskDisplay

--- src/components/TaskDisplay/View.js
+++ src/components/TaskDisplay/View.js
@@ -2,16 +2,42 @@ import React from 'react';
 import TabList from '../TabList';
 import TabDisplay from '../TabDisplay';
 import './TaskDisplay.css';
-import Calendar from 'react-calendar'
+import BigCalendar from 'react-big-calendar-like-google';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
 
-const View = ({}) => (
 
+const localizer = BigCalendar.momentLocalizer(moment)
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
+
+const View = props => {
+  const myEventsList = [
+    {
+      allDay: false,
+      end: new Date('December 10, 2017 11:13:00'),
+      start: new Date('December 09, 2017 11:13:00'),
+      title: 'hi',
+    },
+    {
+      allDay: true,
+      end: new Date('December 09, 2017 11:13:00'),
+      start: new Date('December 09, 2017 11:13:00'),
+      title: 'All Day Event',
+    },
+  ];
+  return (
     <div id="taskdisplay">
-        <TabList />
-        <TabDisplay />
-        <Calendar></Calendar>
-    </div>
-
-);
+    <TabList />
+      <TabDisplay />
+        <div className="rbc-calendar">
+        <BigCalendar
+        events={myEventsList}
+        views={allViews}
+        startAccessor='startDate'
+        endAccessor='endDate'
+        localizer='localizer'
+        />
+        </div>
+    </div>)}
 
 export default View;
