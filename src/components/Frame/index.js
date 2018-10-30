c5a6b9d5ad1bbb30761011a2315c88060fb94c73
janeon <jhsieh@oberlin.edu>
2018-10-29 20:42:28 -0400

    moving calendar into its own component

--- src/components/Frame/index.js
+++ src/components/Frame/index.js
@@ -2,11 +2,10 @@
 import React, { Component }  from 'react';
 import View from './View';
 export default class Frame extends Component {
-// why is there a view file/component on top of the index.js for the frame component itself?
+
     render() {
         return (
             <View tasks={this.props.tasks}/>
         );
     }
-
 }
