9a8ae76d38a82f769b4c02d06622230654e24afc
Adina Johnson <adinacjohnson@gmail.com>
2018-10-30 14:28:04 -0400

    journal skeleton component

---
+++ src/components/Journal/View.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const View = ({}) => (
+    <div className="displayContainer">
+        
+    </div>
+);
+
+export default View;
---
+++ src/components/Journal/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import View from './View';
+
+class Journal extends Component {
+
+    render() {
+        return <View />;
+    }
+
+}
+
+export default Journal;
