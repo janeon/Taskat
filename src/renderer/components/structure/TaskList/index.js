66416f2a76d8c4b6747a2231f7b0dbaa2103449a
janeon <jhsieh@oberlin.edu>
2018-11-27 12:23:31 -0500

    adding currentTask css tag

--- src/renderer/components/structure/TaskList/index.js
+++ src/renderer/components/structure/TaskList/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import View from "./View"
 
 /*
- * Renders the list of tasks, and also creates the button for adding new tasks.  
+ * Renders the list of tasks, and also creates the button for adding new tasks.
  */
 class TaskList extends Component {
 
     constructor(props) {
         super(props);
-        
+
         // farthest down the model should go...
         this.model = props.model;
         // setting the initial state
@@ -18,21 +18,21 @@ class TaskList extends Component {
     }
 
     /*
-     * This is a lifecycle method from React.Component that gets called after this 
-     * component is loaded into the DOM.  
+     * This is a lifecycle method from React.Component that gets called after this
+     * component is loaded into the DOM.
      */
     componentDidMount() {
         // The TaskList wants to know when the list of tasks changes.
         this.model.subscribeTo(this, "title_key_list");
     }
 
-    // Another lifecycle method, that gets called before it is removed from the DOM. 
+    // Another lifecycle method, that gets called before it is removed from the DOM.
     componentWillUnmount() {
         this.model.unsubscribeFrom(this, "title_key_list");
     }
 
     /*
-     * When the list of title/keys changes, this component should update its state to reflect 
+     * When the list of title/keys changes, this component should update its state to reflect
      * the new titlekey list.
      */
     onChange(newTitleKeyList) {
@@ -43,22 +43,35 @@ class TaskList extends Component {
     }
 
     /*
-     * Update the current task in the model on task element clicks. 
+     * Update the current task in the model on task element clicks.
      */
     onClick(key) {
         this.model.updateCurrentTask(parseInt(key));
     }
 
     render() {
-
+      var data = this.model.resources.currentTask.data; // current task data
         // converting task objects to task html elements
         const taskTitleElementList = this.state.titleKeyList.map((titleKeyPair) => {
             const key = titleKeyPair.key;
-            return <div className="task" 
+            console.log("current task data,", data.key);
+            console.log("key", key);
+            var ret;
+            if (data.key === key) {
+              ret = <div className="currentTask"
+                          onClick={(e) => this.onClick(key)}
+                          key={titleKeyPair.key}>
+                              {titleKeyPair.title}
+                      </div>;
+            }
+            else {
+              ret = <div className="task"
                         onClick={(e) => this.onClick(key)}
                         key={titleKeyPair.key}>
                             {titleKeyPair.title}
                     </div>;
+              }
+            return ret;
         });
 
         return <View createTask={this.model.createTask} taskTitleElementList={taskTitleElementList}/>;
