ca16a21ddf3b1eeaececf6f366ab9752414ea7bb
svouse <svouse@oberlin.edu>
2018-12-01 22:56:19 -0500

    fixed conflicts with passing of props modified for taskedit

--- src/renderer/components/structure/TaskList/View.js
+++ src/renderer/components/structure/TaskList/View.js
@@ -8,10 +8,8 @@ import "./TaskList.css";
 // 'taskElementList' is a list of html elements that represent tasks.
 const View = ({taskTitleElementList,deleteButton, newTaskButton}) => (
     <div id="tasklist-container">
-     <button className="deleteButton" onClick={createTask}> Delete Task </button>
-            <div className="task" id="new-task-button-container">
-                <NewTaskButton createTask={createTask}/>
-            </div>
+            {deleteButton}
+            {newTaskButton}
             <div id="task-list">
                 {taskTitleElementList}
             </div>
